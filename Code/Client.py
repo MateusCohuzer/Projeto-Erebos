567f2a2d0a2e1a7c5aeb86af30a6746f3c571919
Mateus <matebatichotti@gmail.com>
2021-05-11 14:23:35 -0300

    Update Client.py

--- Code/Client.py
+++ Code/Client.py
@@ -1,18 +1,31 @@
 import socket
+import time
 
 HOST = '127.0.0.1'
 PORT = 5000
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
-
 controle = '0'
+
+while True:
+    try:
+        udp.connect(dest)
+    finally:
+        start = time.perf_counter()
+        print('Client conectado ao servidor!')
+        break
+
 print(f'Para sair digite: {controle}\n')
 msg = input().encode('utf-8')
 
 while True:
+    if msg.decode('utf-8') == controle:
+        break
     udp.sendto(msg, dest)
     msg = input().encode('utf-8')
-
 udp.close()
+
+finish = time.perf_counter()
+print(f'\033[1;33mClient desconectado\n-> O cliente ficou conectado por {round(finish-start), 2} segundos.')
 print('Fim do processo')
