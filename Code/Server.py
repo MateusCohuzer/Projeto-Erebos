4b86c2b58b9e4f40afef5d60a5a006308dddbc8d
Mateus <matebatichotti@gmail.com>
2021-05-12 18:54:58 -0300

    fix Stream bug

--- Code/Server.py
+++ Code/Server.py
@@ -3,19 +3,28 @@ import threading
 
 
 def aceitaClientes():
+    global controle_thread, udp
     while True:
-        clientsocket, address = udp.accept()
-        print(f'Connection from {address} has been established!')
-        clientsocket.send(bytes("Bem vindo ao Servidor!", 'utf-8'))
+        try:
+            clientsocket, address = udp.accept()
+            global clientsocket, address
+        finally:
+            controle_thread += 1
+            print(f'Connection from {address} has been established!')
+            clientsocket.send(bytes("Bem vindo ao Servidor!", 'utf-8'))
 
 
 PORT = 5000
 BUFFSIZE = 1024
+controle_thread = 0
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (socket.gethostname(), PORT)
 udp.bind(orig)
 udp.listen(3)
+
+thread_aceitaClientes = threading.Thread(target=aceitaClientes)
+thread_aceitaClientes.start()
 
 while True:
     try:
