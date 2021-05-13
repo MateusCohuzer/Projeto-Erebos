783a9df3ec67db56a1b450b2cad65b48f39dc1e4
Mateus <matebatichotti@gmail.com>
2021-05-13 14:10:29 -0300

    Update Server.py

--- Code/Server.py
+++ Code/Server.py
@@ -1,3 +1,57 @@
 import socket
-import threading
+from threading import Thread
 
+
+def accept_connections():
+    while True:
+        client, client_address = SERVER.accept()
+        print(f'{client_address} está online.')
+        client.send(bytes("Qual o seu nome ?", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):
+    name = client.recv(BUFSIZ).decode("utf8")
+    client.send(bytes(f'Welcome, {name}!', "utf8"))
+    client.send(bytes("Agora você pode enviar mensagens !", "utf8"))
+    msg = f'{name} entrou no chat!'
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name + "")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes(f"{name} saiu do chat", "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+
+clients = {}
+addresses = {}
+
+HOST = ""
+PORT = 65000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+
+SERVER.close()
