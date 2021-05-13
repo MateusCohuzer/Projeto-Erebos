c158b765c20d5b1140b831512fbcae49332005b3
Mateus <matebatichotti@gmail.com>
2021-05-13 13:48:01 -0300

    Hakai

--- Code/Client.py
+++ Code/Client.py
@@ -1,30 +1,4 @@
 import socket
+import threading
 import time
 
-PORT = 5000
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (socket.gethostname(), PORT)
-controle = '0'
-
-while True:
-    try:
-        tcp.connect(dest)
-    finally:
-        start = time.perf_counter()
-        print('Client conectado ao servidor!')
-        break
-
-print(f'Para sair digite: {controle}\n')
-msg = input().encode('utf-8')
-
-while True:
-    if msg.decode('utf-8') == controle:
-        break
-    tcp.sendto(msg, dest)
-    msg = input().encode('utf-8')
-tcp.close()
-
-finish = time.perf_counter()
-print(f'\033[1;33mCliente desconectado\n-> O cliente ficou conectado por {round(finish-start, 2)} segundos.')
-print('Fim do processo')
--- Code/Server.py
+++ Code/Server.py
@@ -1,42 +1,4 @@
 import socket
 import threading
+import time
 
-
-def aceitaClientes():
-    global controle_thread, tcp, limite_conexoes, clientsocket, address
-    while True:
-        if controle_thread < limite_conexoes:
-            try:
-                clientsocket, address = tcp.accept()
-            finally:
-                controle_thread += 1
-                print(f'Connection from {address} has been established!')
-                clientsocket.send(bytes("Bem vindo ao Servidor!", 'utf-8'))
-        else:
-            print('O limite de conexões foi feito.')
-            break
-
-PORT = 5000
-BUFFSIZE = 1024
-controle_thread = 0
-limite_conexoes = 3
-
-clientsocket = ''
-address = ''
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (socket.gethostname(), PORT)
-tcp.bind(orig)
-tcp.listen(limite_conexoes)
-
-thread_aceitaClientes = threading.Thread(target=aceitaClientes)
-thread_aceitaClientes.start()
-
-while True:
-    try:
-        msg, cliente = tcp.recvfrom(BUFFSIZE)
-        msg.decode('utf-8')
-        print(f'{cliente}: {msg}')
-    except:
-        break
-tcp.close()
