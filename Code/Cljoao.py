941a0df0cfcb8c35a1aa985edd772a47a616faa3
Mateus <matebatichotti@gmail.com>
2021-05-16 10:40:26 -0300

    Update Cljoao.py

--- Code/Cljoao.py
+++ Code/Cljoao.py
@@ -1,30 +1,42 @@
+# EXECUTAR COM PYCHARM PARA VER OS NOMES COLORIDOS
 import socket
 import threading
 from time import sleep
 from random import randint
 
+
 def reciveMsg():
     while True:
         msgBytes, serverIP = client.recvfrom(2048)
         print('\n' + msgBytes.decode('utf8'))
+        if kill_bool:
+            break
+        client.close()
 
 
 def clientSide(address):
-    global cont
+    global cont, kill_var, kill_bool
     while True:
         if cont == 0:
             msgSend = input("Name: ")
-            msgSend = f'\033[1;3{randint(1,6)}m{msgSend}\033[m'
+            msgSend = f'\033[1;3{randint(1, 6)}m{msgSend}\033[m'
         else:
             sleep(0.0001)
             msgSend = input("MSG: ").capitalize()
-
+            if msgSend == kill_var:
+                msgSend = f'\033[1;31m >>USUÁRIO SE DESCONECTOU \033[m'
+                kill_bool = True
         msgSend = str(cont) + msgSend
         client.sendto(msgSend.encode('utf8'), address)
         cont += 1
+        if kill_bool:
+            break
+    client.close()
 
 
 cont = 0
+kill_var = '/exit'
+kill_bool = False
 HOST = "192.168.0.106"
 PORT = 12000
 ADDR = (HOST, PORT)
